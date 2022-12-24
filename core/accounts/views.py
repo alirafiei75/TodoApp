b0dc9af845c2f20743028b4dd3ccf84c11665a19
ali rafiei <103249712+alirafiei75@users.noreply.github.com>
2022-12-24 20:25:40 +0330

    final edit

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -6,10 +6,13 @@ from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 class CustomLoginView(LoginView):
+    """class for custom login view"""
     template_name = 'accounts/login.html'
     redirect_authenticated_user = True
 
-    def dispatch(self, request, *args, **kwargs):       
+    def dispatch(self, request, *args, **kwargs):
+        """custom dispatch
+        to redirect authenticated user to home page"""       
         if request.user.is_authenticated:
             return redirect('todo:tasks')
         return super(CustomLoginView, self).dispatch(request, *args, **kwargs)
@@ -19,10 +22,12 @@ class CustomLoginView(LoginView):
 
 
 class CustomLogoutView(LoginRequiredMixin, LogoutView):
+    """class for custom logout view"""
     template_name = 'accounts/logout.html'
 
 
 class SignupView(CreateView):
+    """class for custom signup view"""
     form_class = UserCreationForm
     success_url = reverse_lazy('accounts:login')
     template_name = 'accounts/signup.html'
--- core/todo/models.py
+++ core/todo/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
+    """class to define task model"""
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
